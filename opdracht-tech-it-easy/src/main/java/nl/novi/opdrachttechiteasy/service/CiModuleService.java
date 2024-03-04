134baa7095f64daaff7aded37486da44292fed3c
MelroyL92 <melroy_lingsma@live.nl>
2024-03-04 19:40:54 +0100

    Adjusted service based on television service

--- opdracht-tech-it-easy/src/main/java/nl/novi/opdrachttechiteasy/service/CiModuleService.java
+++ opdracht-tech-it-easy/src/main/java/nl/novi/opdrachttechiteasy/service/CiModuleService.java
@@ -1,4 +1,65 @@
 package nl.novi.opdrachttechiteasy.service;
+import jakarta.persistence.EntityNotFoundException;
+import nl.novi.opdrachttechiteasy.exceptions.RecordNotFoundException;
+import nl.novi.opdrachttechiteasy.models.CiModule;
+import nl.novi.opdrachttechiteasy.repositories.CiModuleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
 public class CiModuleService {
+
+    @Autowired
+    private final CiModuleRepository ciModuleRepository;
+
+
+    public CiModuleService(CiModuleRepository ciModuleRepository){
+        this.ciModuleRepository = ciModuleRepository;
+    }
+
+    public List<CiModule> getCiModule() {
+        return ciModuleRepository.findAll();
+    }
+
+    public CiModule getCimodule(long id){
+        Optional<CiModule> optionalCiModule = ciModuleRepository.findById(id);
+        if (optionalCiModule.isEmpty()) {
+            throw new RuntimeException();
+        } else {
+            return optionalCiModule.get();
+        }
+    }
+
+    public CiModule ciModule(CiModule ciModule){
+        return ciModuleRepository.save(ciModule);
+    }
+
+
+    public void updateCiModule(Long id, CiModule ciModule) {
+        Optional<CiModule>CiModuleFound = ciModuleRepository.findById(id);
+        if(CiModuleFound.isPresent()){
+            CiModule excistingCiModule = CiModuleFound.get();
+            excistingCiModule.setPrice(ciModule.getPrice());
+            excistingCiModule.setName(ciModule.getName());
+            excistingCiModule.setType(ciModule.getType());
+            excistingCiModule.setId(ciModule.getId());
+            ciModuleRepository.save(excistingCiModule);
+        } else {
+            throw new RecordNotFoundException("CI module with ID " + id + " does not exist");
+        }
+    }
+
+    public void removeCiModule (Long ciModuleId) {
+        System.out.println("the value of the id = " + ciModuleId);
+        if (!ciModuleRepository.existsById(ciModuleId)) {
+            throw new EntityNotFoundException("Television with ID " + ciModuleId + " not found");
+        }
+        ciModuleRepository.deleteById(ciModuleId);
+    }
+
+
 }
--- opdracht-tech-it-easy/src/main/java/nl/novi/opdrachttechiteasy/service/RemoteControllerService.java
+++ opdracht-tech-it-easy/src/main/java/nl/novi/opdrachttechiteasy/service/RemoteControllerService.java
@@ -1,4 +1,63 @@
 package nl.novi.opdrachttechiteasy.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import nl.novi.opdrachttechiteasy.exceptions.RecordNotFoundException;
+import nl.novi.opdrachttechiteasy.models.RemoteController;
+import nl.novi.opdrachttechiteasy.repositories.RemoteControllerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class RemoteControllerService {
+
+    @Autowired
+    private final RemoteControllerRepository remoteControllerRepository;
+
+    public RemoteControllerService(RemoteControllerRepository remoteControllerRepository) {
+        this.remoteControllerRepository = remoteControllerRepository;
+    }
+
+    public List<RemoteController> getRemoteController() {
+        return remoteControllerRepository.findAll();
+    }
+
+    public RemoteController getRemoteController(long id){
+        Optional<RemoteController> optionalRemoteController = remoteControllerRepository.findById(id);
+        if (optionalRemoteController.isEmpty()) {
+            throw new RuntimeException();
+        } else {
+            return optionalRemoteController.get();
+        }
+    }
+
+    public RemoteController remoteController(RemoteController remoteController){
+        return remoteControllerRepository.save(remoteController);
+    }
+
+    public void updateRemoteController(Long id, RemoteController remoteController) {
+        Optional<RemoteController>RemoteFound = remoteControllerRepository.findById(id);
+        if(RemoteFound.isPresent()){
+            RemoteController excistingRemoteController = RemoteFound.get();
+            excistingRemoteController.setPrice(remoteController.getPrice());
+            excistingRemoteController.setName(remoteController.getName());
+            excistingRemoteController.setId(remoteController.getId());
+            excistingRemoteController.setOriginalStock(remoteController.getOriginalStock());
+            excistingRemoteController.setBatteryType(remoteController.getBatteryType());
+            excistingRemoteController.setCompatibleWith(remoteController.getCompatibleWith());
+            remoteControllerRepository.save(remoteController);
+        } else {
+            throw new RecordNotFoundException("Remotecontroller with ID : " + id + " does not exist");
+        }
+    }
+
+    public void removeRemoteController (Long remoteControllerId) {
+        System.out.println("the value of the id = " + remoteControllerId);
+        if (!remoteControllerRepository.existsById(remoteControllerId)) {
+            throw new EntityNotFoundException("Television with ID " + remoteControllerId + " not found");
+        }
+        remoteControllerRepository.deleteById(remoteControllerId);
+    }
 }
